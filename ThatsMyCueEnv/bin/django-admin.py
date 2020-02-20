dad61ddbae3db03153a817148c3852189eb9b7e7
John Long <jlcomp45@gmail.com>
2020-02-19 20:56:32 -0600

    completed models

---
+++ ThatsMyCueEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jlcomp45/workspace/django_thats_my_cue/ThatsMyCueEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ django_thats_my_cueapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoThatsMyCueapiConfig(AppConfig):
+    name = 'django_thats_my_cueapi'
